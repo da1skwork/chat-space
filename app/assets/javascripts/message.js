7d4c9c38865a4cceb8b524bc44109050ca88bbc5
根本大輔 <defmono112233@gmail.com>
2018-01-23 07:53:00 +0900

    [update]メッセージ送信機能の非同期通信化

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,45 @@
+$(function(){
+  function buildHTML(message){
+    var html = `<li class= "message-list">
+                          <p class= "message-list__username">
+                            ${message.user_name}
+                          </p>
+                          <p class= "message-list__date">
+                            format_posted_time(${message.created_at})
+                          </p>
+                          if ${message.content}.present?
+                            <p class= "message-list__comment">
+                              ${message.content}
+                            </p>
+                          end
+                          <img ${message.image.url}, class= "message-list__image" if ${message.image}.present?
+                </li>`
+    return html;
+  };
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')]
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form__message').val("");
+      $('.hidden').val("");
+      $('html,body').animate({
+        scrollTop: $(document).height()});
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+})
