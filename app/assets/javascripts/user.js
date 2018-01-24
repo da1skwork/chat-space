ed3221dfbc85b310cf9eac628e72d0b7df336f87
根本大輔 <defmono112233@gmail.com>
2018-01-24 15:27:58 +0900

    [fix]レビュー指摘箇所の修正

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -4,16 +4,16 @@ var add_member = $(".chat-group-form__field--right__member");
 
 
 function appendUser(user){
-  var html= `<div class="chat-group-user clearfix ${user.name}">
-              <p class="chat-group-user__name"> ${ user.name }</p>
-              <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
-            </div>`
+  var html = `<div class="chat-group-user clearfix ${ user.name }">
+                <p class="chat-group-user__name"> ${ user.name }</p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
+              </div>`
 
   search_list.append(html);
 }
 
 function appendMember(user_name,user_id) {
-    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+  var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
                   <input name='group[user_ids][]' type='hidden' value='${ user_id }'>
                   <p class='chat-group-user__name'>${ user_name }</p>
                   <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
@@ -43,16 +43,15 @@ function appendMember(user_name,user_id) {
     });
   });
 
-  $('.user-search-result').on('click','a',function(){
-    var user_name = $(this).attr('data-user-name');
-    var user_id = $(this).attr('data-user-id');
-    $(this).parent().remove();
-      appendMember(user_name, user_id);
+  $('.user-search-result').on('click','.chat-group-user',function(){
+    var user_name = $('.user-search-add').attr('data-user-name');
+    var user_id = $('.user-search-add').attr('data-user-id');
+    $(this).remove();
+    appendMember(user_name, user_id);
   });
 
-  $('.chat-group-form__field--right__member').on('click','a',function(){
-    console.log($(this).parent());
-    $(this).parent().remove();
+  $('.chat-group-form__field--right__member').on('click','#chat-group-user-8',function(){
+    $(this).remove();
   });
 
 });
